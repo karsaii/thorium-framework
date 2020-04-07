90b4fa21c22bcfafe7e26570fe11e85f5b811fc8
István Karsai <istvan.karsai.public@gmail.com>
2020-04-08 01:32:36 +0200

    Adding an extra driver wait function, and changing unintended behavior.

--- src/main/java/selenium/namespaces/DriverWaits.java
+++ src/main/java/selenium/namespaces/DriverWaits.java
@@ -2,7 +2,7 @@ package selenium.namespaces;
 
 import core.extensions.interfaces.DriverFunction;
 import core.extensions.namespaces.BasicPredicateFunctions;
-import core.namespaces.DataFunctions;
+import core.namespaces.Executor;
 import core.namespaces.WaitTimeDataFactory;
 import core.records.WaitData;
 import selenium.constants.DataConstants;
@@ -14,20 +14,19 @@ import static selenium.namespaces.ExecutionCore.ifDriver;
 public interface DriverWaits {
     static DriverFunction<Boolean> waitNavigatedTo(String url, int interval, int timeout) {
         return ifDriver(
-            "waitNavigatedTo",
-            isNotBlank(url) && areAll(BasicPredicateFunctions::isPositiveNonZero, interval, timeout) && (interval < timeout),
-            driver -> {
-                final var result = Driver.navigate(url).apply(driver);
-                return DataFunctions.isValidNonFalse(result) ? Wait.untilCore(
-                    new WaitData<>(
-                        EC.isUrlContainsData(url),
-                        WaitPredicateFunctions::isTruthyData,
-                        "Waiting for url",
-                        WaitTimeDataFactory.getWithDefaultClock(interval, timeout)
-                    )
-                ).apply(driver) : result;
-            },
-            DataConstants.PARAMETERS_NULL_BOOLEAN
+                "waitNavigatedTo",
+                isNotBlank(url) && areAll(BasicPredicateFunctions::isPositiveNonZero, interval, timeout) && (interval < timeout),
+                driver -> Wait.untilCore(new WaitData<>(EC.isUrlContainsData(url), WaitPredicateFunctions::isTruthyData, "Waiting for url", WaitTimeDataFactory.getWithDefaultClock(interval, timeout))).apply(driver),
+                DataConstants.PARAMETERS_NULL_BOOLEAN
+        );
+    }
+
+    static DriverFunction<Boolean> navigateAndWait(String url, int interval, int timeout) {
+        return ifDriver(
+                "navigateAndWait",
+                isNotBlank(url) && areAll(BasicPredicateFunctions::isPositiveNonZero, interval, timeout) && (interval < timeout),
+                Executor.execute(Driver.navigate(url), waitNavigatedTo(url, interval, timeout)),
+                DataConstants.PARAMETERS_NULL_BOOLEAN
         );
     }
 }
