978cf2d912985f18de9fe33f4acedf8e329296cd
Istvan Karsai <istvan.karsai.public@gmail.com>
2020-04-07 10:52:09 +0200

    Removing previous refactor leftovers. (#3)

--- src/main/java/data/IContainedData.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-import java.util.function.Function;
-
-@FunctionalInterface
-public interface IContainedData<T, U> extends IContained<T, Data<U>> {
-    Function<T, Data<U>> get();
-}
--- src/main/java/data/IElement.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-@FunctionalInterface
-public interface IElement extends IContainedData<WebDriver, WebElement> {
-    DriverFunction<WebElement> get();
-}
